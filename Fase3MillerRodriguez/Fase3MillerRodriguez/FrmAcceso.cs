25b393dc4d6f8293253bf971663dc44c7998c916
merodriguezti <78769885+merodriguezti@users.noreply.github.com>
2021-11-20 23:52:16 -0500

    V1.01.20112021
    inicio del proyecto

---
+++ Fase3MillerRodriguez/Fase3MillerRodriguez/FrmAcceso.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fase3MillerRodriguez
+{
+    public partial class FrmAcceso : Form
+    {
+        public FrmAcceso()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmAcceso_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
